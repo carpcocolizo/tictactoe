e8d557194f426ca3e9184cd045ba99cb6a740abb
carpcocolizo <germanvalente@yahoo.com>
2021-06-06 13:14:02 +0200

    New versions, reworked code

--- readme.md
+++ readme.md
@@ -1,11 +1,19 @@
 ## README
-Hey, ok, this was a little more of a challenge
-I think the code is not difficult to write itself
-Is pretty easy to think about tictactoe and what has to happen and in what order
-So the logic is simple
-The problem is with the Classes, modules, methods, scopes
-I will upload my solution, I didnt look to any other code
-So after I upload this I will check the solution in TOP and take a look to the walkthrough too
-But what can I say, my game works
-Is not pretty code
-And it should have a different order!
+## Hey, ok, this was a little more of a challenge
+## I think the code is not difficult to write itself
+## Is pretty easy to think about tictactoe and what has to happen and in what order
+## So the logic is simple
+## The problem is with the Classes, modules, methods, scopes
+## I will upload my solution, I didnt look to any other code
+## So after I upload this I will check the solution in TOP and take a look to the walkthrough too
+## But what can I say, my game works
+## Is not pretty code
+## And it should have a different order!
+
+## I changed a few things
+## I created a new class call NewGame
+## I initialize a new game from there, I think it make it easier to read
+## Some methods maybe belong to another class
+## But in general the code is much cleaner
+## Now it ask at the end if you want to play again
+## It could be improve a little more, but for now it will stay this way 
--- tictactoe.rb
+++ tictactoe.rb
@@ -7,11 +7,11 @@ module Board
 
   def current_board
     puts(<<-EOT)
-        #{@@positions[0]} | #{@@positions[1]} | #{@@positions[2]}
-      -------------
-        #{@@positions[3]} | #{@@positions[4]} | #{@@positions[5]}
-      -------------
-        #{@@positions[6]} | #{@@positions[7]} | #{@@positions[8]}
+          #{@@positions[0]} | #{@@positions[1]} | #{@@positions[2]}
+        -------------
+          #{@@positions[3]} | #{@@positions[4]} | #{@@positions[5]}
+        -------------
+          #{@@positions[6]} | #{@@positions[7]} | #{@@positions[8]}
     EOT
   end
 
@@ -40,34 +40,43 @@ module Board
       @@positions[play - 1] = marker
       puts current_board
       checkwinner
+      tie
     end
   end
 
   def checkwinner
     if @@positions[0] == 'X' && @@positions[1] == 'X' && @@positions[2] == 'X' || @@positions[0] == 'O' && @@positions[1] == 'O' && @@positions[2] == 'O'
       puts "#{name} YOU ARE THE WINNER!"
-      resetboard
+      resetboard; askforplay
     elsif @@positions[3] == 'X' && @@positions[4] == 'X' && @@positions[5] == 'X' || @@positions[3] == 'O' && @@positions[4] == 'O' && @@positions[5] == 'O'
       puts "#{name} YOU ARE THE WINNER!"
-      resetboard
+      resetboard; askforplay
     elsif @@positions[6] == 'X' && @@positions[7] == 'X' && @@positions[8] == 'X' || @@positions[6] == 'O' && @@positions[7] == 'O' && @@positions[8] == 'O'
       puts "#{name} YOU ARE THE WINNER!"
-      resetboard
+      resetboard; askforplay
     elsif @@positions[0] == 'X' && @@positions[4] == 'X' && @@positions[8] == 'X' || @@positions[0] == 'O' && @@positions[4] == 'O' && @@positions[8] == 'O'
       puts "#{name} YOU ARE THE WINNER!"
-      resetboard
+      resetboard; askforplay
     elsif @@positions[6] == 'X' && @@positions[4] == 'X' && @@positions[2] == 'X' || @@positions[6] == 'O' && @@positions[4] == 'O' && @@positions[2] == 'O'
       puts "#{name} YOU ARE THE WINNER!"
-      resetboard
+      resetboard; askforplay
     elsif @@positions[0] == 'X' && @@positions[3] == 'X' && @@positions[6] == 'X' || @@positions[0] == 'O' && @@positions[3] == 'O' && @@positions[6] == 'O'
       puts "#{name} YOU ARE THE WINNER!"
-      resetboard
+      resetboard; askforplay
     elsif @@positions[1] == 'X' && @@positions[4] == 'X' && @@positions[7] == 'X' || @@positions[1] == 'O' && @@positions[4] == 'O' && @@positions[7] == 'O'
       puts "#{name} YOU ARE THE WINNER!"
-      resetboard
+      resetboard; askforplay
     elsif @@positions[2] == 'X' && @@positions[5] == 'X' && @@positions[8] == 'X' || @@positions[2] == 'O' && @@positions[5] == 'O' && @@positions[8] == 'O'
       puts "#{name} YOU ARE THE WINNER!"
-      resetboard
+      resetboard; askforplay
+    end
+  end
+
+  def tie
+    if @@positions.any?(Integer)
+    else
+      puts 'IS A FUCKING TIE'
+      resetboard; askforplay
     end
   end
 
@@ -87,118 +96,98 @@ class Player
   end
 end
 
-def newplayers
-  puts 'Hey pal, what is your name?'
-  name = gets.chop
-  puts 'You will play with a random marker between X and O'
-  random = rand(10)
-  marker = random.even? ? 'X' : 'O'
-  sleep(2)
-  puts 'Beep beep'
-  sleep(2)
-  puts 'Tossing a coin for you'
-  sleep(2)
-  puts "Your marker is #{marker}"
-  sleep(2)
-  if marker == 'X'
-    puts 'You play first'
-  else
-    puts 'You play in second'
+class NewGame < Player
+  attr_reader :name, :marker
+
+  def initialize
+    @name = getplayer1name
+    @marker = getplayer1marker
+    @name2 = getplayer2name
+    @marker2 = getplayer2marker
   end
-  player1 = Player.new(name, marker)
-  sleep(2)
-  puts 'Alright, so the name of the other pal is?'
-  name2 = gets.chop
-  sleep(2)
-  if marker == 'O'
-    marker2 = 'X'
-    puts "Because #{name} is playing with #{marker}, you will be #{marker2} and go first!"
-  else
-    marker2 = 'O'
-    puts "Because #{name} is playing with #{marker}, you will be #{marker2} and go in second"
+
+  def getplayer1name
+    puts 'Hey pal, what is your name?'
+    name = gets.chop
+    puts 'You will play with a random marker between X and O'
+    name
   end
-  player2 = Player.new(name2, marker2)
-  p player1
-  p player2
-end
 
-def getplayer1name
-  puts 'Hey pal, what is your name?'
-  name = gets.chop
-  puts 'You will play with a random marker between X and O'
-  name
-end
-
-def getplayer1marker
-  random = rand(10)
-  marker = random.even? ? 'X' : 'O'
-  sleep(2)
-  puts 'Beep beep'
-  sleep(2)
-  puts 'Tossing a coin for you'
-  sleep(2)
-  puts "Your marker is #{marker}"
-  sleep(2)
-  if marker == 'X'
-    puts 'You play first'
-  else
-    puts 'You play in second'
+  def getplayer1marker
+    random = rand(10)
+    marker = random.even? ? 'X' : 'O'
+    sleep(2)
+    puts 'Beep beep'
+    sleep(2)
+    puts 'Tossing a coin for you'
+    sleep(2)
+    puts "Your marker is #{marker}"
+    sleep(2)
+    if marker == 'X'
+      puts 'You play first'
+    else
+      puts 'You play in second'
+    end
+    marker
   end
-  marker
-end
 
-def getplayer2name
-  sleep(2)
-  puts 'Alright, so the name of the other pal is?'
-  gets.chop
-end
-
-def getplayer2marker
-  sleep(2)
-  if marker == 'O'
-    marker2 = 'X'
-    puts "Because #{name} is playing with #{marker}, you will be #{marker2} and go first!"
-  else
-    marker2 = 'O'
-    puts "Because #{name} is playing with #{marker}, you will be #{marker2} and go in second"
+  def getplayer2name
+    sleep(2)
+    puts 'Alright, so the name of the other pal is?'
+    gets.chop
   end
-  marker2
+
+  def getplayer2marker
+    sleep(2)
+    if @marker == 'O'
+      marker2 = 'X'
+      puts "Because #{@name} is playing with #{@marker}, you will be #{marker2} and go first!"
+    else
+      marker2 = 'O'
+      puts "Because #{@name} is playing with #{@marker}, you will be #{marker2} and go in second"
+    end
+    marker2
+  end
+
+  def play
+    player1 = Player.new(@name, @marker)
+    player2 = Player.new(@name2, @marker2)
+
+    if player1.marker == 'X'
+      player1.firstmove
+      player2.keeponplaying
+      player1.keeponplaying
+      player2.keeponplaying
+      player1.keeponplaying
+      player2.keeponplaying
+      player1.keeponplaying
+      player2.keeponplaying
+      player1.keeponplaying
+    else
+      player2.firstmove
+      player1.keeponplaying
+      player2.keeponplaying
+      player1.keeponplaying
+      player2.keeponplaying
+      player1.keeponplaying
+      player2.keeponplaying
+      player2.keeponplaying
+      player1.keeponplaying
+    end
+  end
 end
 
-name = getplayer1name
-marker = getplayer1marker
-name2 = getplayer2name
-marker2 = ''
-sleep(2)
-if marker == 'O'
-  marker2 = 'X'
-  puts "Because #{name} is playing with #{marker}, you will be #{marker2} and go first!"
-else
-  marker2 = 'O'
-  puts "Because #{name} is playing with #{marker}, you will be #{marker2} and go in second"
+def askforplay
+  puts 'You Wanna play again? (Y/N)'
+  ans = gets.chop
+  case ans
+  when 'Y', 'y'
+    NewGame.new.play
+  when 'N', 'n'
+    puts 'Thanks for playing'
+  else
+    puts 'INVALID ANSWER'
+    askforplay
+  end
 end
-
-player1 = Player.new(name, marker)
-player2 = Player.new(name2, marker2)
-
-if player1.marker == 'X'
-  player1.firstmove
-  player2.keeponplaying
-  player1.keeponplaying
-  player2.keeponplaying
-  player1.keeponplaying
-  player2.keeponplaying
-  player1.keeponplaying
-  player2.keeponplaying
-  player1.keeponplaying
-else
-  player2.firstmove
-  player1.keeponplaying
-  player2.keeponplaying
-  player1.keeponplaying
-  player2.keeponplaying
-  player1.keeponplaying
-  player2.keeponplaying
-  player1.keeponplaying
-  player2.keeponplaying
-end
+NewGame.new.play
